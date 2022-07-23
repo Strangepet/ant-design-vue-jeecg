aaa683c8bd2029026fd0deee9883b55f45f98f17
Strangepet <1912588566@qq.com>
2022-07-23 16:47:02 +0800

    嵌入form-making

---
+++ src/views/form-making/form-making.vue
@@ -0,0 +1,32 @@
+<template>
+  <fm-making-form 
+    ref="makingform" 
+    style="height: 600px;" 
+    preview 
+    generate-code 
+    generate-json
+  >
+    <template slot="action">
+    </template>
+  </fm-making-form>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+              formName:'',
+            }
+        },
+        components:{  },
+        methods:{
+            handleSubmit () {
+                const json = this.$refs.makingform.getJSON();
+                console.log(json) //这个就是生成的表单数据，可以直接给后台或者直接拿过来生成表单
+            }
+        },
+        created() {
+            
+        }
+    }
+</script>
